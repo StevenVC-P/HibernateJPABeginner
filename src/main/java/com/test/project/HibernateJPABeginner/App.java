0eb1339cc6e36032fc9042d8dd57bac632e3a47d
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2022-03-09 15:43:44 -0600

    Added templates for query examples

---
+++ src/main/java/com/test/project/HibernateJPABeginner/App.java
@@ -0,0 +1,24 @@
+package com.test.project.HibernateJPABeginner;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.test.hib.controller.findUser_Hql;
+import com.test.hib.model.User;
+public class App
+{
+    public static void main( String[] args )
+    {
+        findUser_Hql u1 = new findUser_Hql();
+        u1.findUser();
+        findUser_Hql u2 = new findUser_Hql();
+        u2.findUserSelect();
+        findUser_Hql u3 = new findUser_Hql();
+        u3.getRecordbyId();
+        findUser_Hql u4 = new findUser_Hql();
+        u4.findUserSelect();
+    }
+
+}
